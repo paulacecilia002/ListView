481f1bcf8d8326e6d60217af7b22f7f15a159bcc
Paula Cecilia <paulacecilia4700@gmail.com>
2026-05-17 12:17:19 -0300

    Primeiro commit

---
+++ lib/Home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista"),
+      ),
+
+      body: Container(
+        padding: const EdgeInsets.all(20),
+
+        child: ListView.builder(
+          itemCount: 10,
+
+          itemBuilder: (context, indice) {
+
+            print("item $indice");
+
+            return ListTile(
+              title: Text("Item $indice"),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
