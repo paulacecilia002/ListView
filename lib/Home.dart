f40f1204c36a95151aad70ad758cde322a5a1603
Paula Cecilia <paulacecilia4700@gmail.com>
2026-05-18 18:47:07 -0300

    descobrir a quantidade de itens dentro da lista

--- lib/Home.dart
+++ lib/Home.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
 
         child: ListView.builder(
 
-          itemCount: _itens.length,
+          itemCount: _itens.length
 
           itemBuilder: (context, indice) {
 
